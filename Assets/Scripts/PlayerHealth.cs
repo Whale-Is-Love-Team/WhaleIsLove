953e5b46a3807d98a04931f057debf3c4e386e70
OuichesDoctor <junkerlucas@gmail.com>
2017-01-21 15:53:31 +0100

    Player health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    [SerializeField]
+    protected int life;
+
+    public void RecieveDamage(int damages) {
+        if (!GameManager.Instance.Running)
+            return;
+
+        life -= damages;
+        if(life < 0) {
+            Debug.Log("Game Over");
+            GameManager.Instance.Running = false;  
+        }
+    }
+}
