28f254a6bc33c69b1f1b90cd17cb66b9cb9fe58b
OuichesDoctor <junkerlucas@gmail.com>
2017-01-21 11:40:04 +0100

    Ennemies : create abstract enemy and projectile + first enemy Diodon

---
+++ Assets/ennemies/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour {
+
+    protected float _projSpeed;
+    protected float _lifeTime;
+    protected Vector2 _direction;
+    protected Rigidbody2D _rb2D;
+
+    public void SetProjSpeed(float speed) {
+        _projSpeed = speed;
+    }
+
+    public void SetLifeTime(float lifeTime) {
+        _lifeTime = lifeTime;
+    }
+
+	void Start () {
+        _direction = gameObject.transform.up;
+        _rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+    void Update() {
+        if (Time.realtimeSinceStartup > _lifeTime)
+            GameObject.Destroy(gameObject);
+    }
+
+	void FixedUpdate () {
+        _rb2D.velocity = _direction * _projSpeed * Time.fixedDeltaTime;
+	}
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.tag == "Player") {
+            Debug.Log("Impact");
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/ennemies/diodon/DiodonEnemy.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class DiodonEnemy : AbstractEnemy {
+
+    // Exposed fields
+    [SerializeField]
+    protected float spikesDamage = 1f;
+    [SerializeField]
+    [Tooltip("time between attacks in seconds")]
+    protected float spikesDelay = 2f;
+    [SerializeField]
+    [Tooltip("time of the attack move")]
+    protected float spikesAttackTime = 1f;
+    [SerializeField]
+    [Tooltip("speed of the projectiles in unit / seconds")]
+    protected float projectileSpeed = 20f;
+    [SerializeField]
+    [Tooltip("number of send projectile")]
+    protected int projectileNumber = 8;
+    [SerializeField]
+    protected float projectileDistance = 40f;
+    [SerializeField]
+    protected GameObject projectile;
+    [SerializeField]
+    protected float slowFactor = 0.1f;
+
+    protected float _nextAttackAt = 0;
+    protected float _staticUntil;
+    protected bool _dontMove = false;
+
+    new void Start() {
+        base.Start();
+        direction = Vector2.left;
+    }
+
+    void Update () {
+        var currentTime = Time.realtimeSinceStartup;
+        if(currentTime > _staticUntil) {
+            _dontMove = false;
+        }
+
+        if(currentTime > _nextAttackAt) {
+            _staticUntil = currentTime + spikesAttackTime;
+            _nextAttackAt = currentTime + spikesDelay;
+            _dontMove = true;
+
+            var arcDivision = 360 / projectileNumber;
+            var projLifeTime = currentTime + projectileDistance / projectileSpeed;
+            for (var i = 0; i < projectileNumber;  i++) {
+                var obj = GameObject.Instantiate(projectile, gameObject.transform.position, Quaternion.Euler(0, 0, i * arcDivision), null);
+                var proj = obj.GetComponent<Projectile>();
+                proj.SetProjSpeed(projectileSpeed);
+                proj.SetLifeTime(projLifeTime);
+            }
+        }
+	}
+
+    void FixedUpdate() {
+        if(!_dontMove) {
+            _rb2d.velocity = direction * moveSpeed * Time.fixedDeltaTime;
+        }
+        else {
+            // @TODO : add slow before attack
+            //var factor = (1f - slowFactor);
+            //if (factor < 0)
+            //    factor = 0;
+            //_rb2d.velocity = _rb2d.velocity * factor;
+
+            _rb2d.velocity = Vector2.zero;
+        }
+
+    }
+}
+    
