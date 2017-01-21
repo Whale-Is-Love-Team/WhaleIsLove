7e4204ab6e368ab4a6865d632593a9de59c4bceb
OuichesDoctor <junkerlucas@gmail.com>
2017-01-21 18:06:41 +0100

    UI : add heart handling

---
+++ Assets/Scripts/UI/LifeDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeDisplay : MonoBehaviour {
+
+    [SerializeField]
+    protected GameObject[] hearts;
+
+    public int life = 3;
+
+	void Update () {
+		for(var i = 0; i < hearts.Length; i++) {
+            if(life > i) {
+                hearts[i].GetComponent<LifeHeart>().isEmpty = false;
+            }
+            else
+                hearts[i].GetComponent<LifeHeart>().isEmpty = true;
+        }
+    }
+}
