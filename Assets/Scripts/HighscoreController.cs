4d434da4b02ed1b5a13cb97a9078aacf3812825d
Remi Laot <remilaot@gmail.com>
2017-01-22 03:45:11 +0100

    ...

--- Assets/Scripts/HighscoreController.cs
+++ Assets/Scripts/HighscoreController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class HighscoreController : MonoBehaviour {
 
+    public Font font;
+    public Material material;
+
+    [SerializeField]
+    private int fontSize = 90;
+    [SerializeField]
+    private Color colorHeader;
+    [SerializeField]
+    private Color color1;
+    [SerializeField]
+    private Color color2;
 
     private int nbCol = 3;
+    private int baseIndex = 0;
     private int maxNbRow = 7;
-    private int[] colX = { -450, 150, 300 };
+    [SerializeField]
+    private int[] colX = { -250, 0, 250 };
+    private string[] colName = { "RANK", "SCORE", "NAME" };
     private int rowHeight = 175;
+    [SerializeField]
+    private int yOffset = 250;
 
 	// Use this for initialization
 	void Start () {
-	}
+        generateList();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,9 +41,32 @@ public class HighscoreController : MonoBehaviour {
         for (int n = 0; n < nbCol; n++)
         {
             int baseX = colX[n];
-            GameObject dummy = new GameObject();
-            dummy.AddComponent<RectTransform>();
-            for (int i = 1; i < )
+            int baseY = yOffset +(rowHeight / 2);
+
+            createText(baseX, baseY, colName[n], colorHeader);
+            for (int i = baseIndex; i < baseIndex + maxNbRow; i++)
+            {
+                if (n == 0) createText(baseX, baseY + ((i - baseIndex) *rowHeight), ""+n, colorHeader);
+            }
         }
+    }
+
+    GameObject createText(int x, int y, string value, Color color)
+    {
+        GameObject dummy = new GameObject();
+        var transformD = dummy.AddComponent<RectTransform>();
+        var rendererD = dummy.AddComponent<CanvasRenderer>();
+        var textD = dummy.AddComponent<UnityEngine.UI.Text>();
+        dummy.transform.SetParent(gameObject.transform);
+        transformD.position = gameObject.transform.position + new Vector3(x, y, 0);
+        textD.text = value;
+        textD.font = font;
+        textD.fontSize = fontSize;
+        textD.horizontalOverflow = HorizontalWrapMode.Overflow;
+        textD.verticalOverflow = VerticalWrapMode.Overflow;
+        textD.alignment = TextAnchor.MiddleCenter;
+        textD.color = color;
+        textD.material = material;
+        return dummy;
     }
 }
