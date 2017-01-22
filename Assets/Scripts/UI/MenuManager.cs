7625fb42006a11b976ac765e1605814bf9f5bdcb
Erwan AZZOUG <erwan.azzoug@etu.univ-nantes.fr>
2017-01-22 13:00:51 +0100

    première version du menu

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void Jouer_OnClick()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void Quitter_OnClick()
+    {
+        Application.Quit();
+    }
+
+    public void Options_OnClick()
+    {
+
+    }
+
+    public void Credits_OnClick()
+    {
+
+    }
+}
